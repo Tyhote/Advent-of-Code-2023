9bc7060fc46a0d297f1f52bfe3ce1be9c6fc125b
Tyhote <hazelmadefresh@gmail.com>
2023-12-01 16:53:59 -0600

    Fixed a bug in the regex

--- Day-1/main.py
+++ Day-1/main.py
@@ -4,7 +4,8 @@ parser = argparse.ArgumentParser("Day-1")
 parser.add_argument("input", help="The file to be processed", type=str)
 
 args = parser.parse_args()
-expression = re.compile(r'(?:(?:one|two|three|four|five|six|seven|eight)(?:teen)?|nine|ten|twenty|\d)')
+# This finds all numbers up to twenty.
+expression = re.compile(r'(?:(?:four|six|seven|nine)(?:teen)?|one|two|three|five|ten|eleven|twelve|thirteen|fifteen|eighteen|eight|twenty|\d)')
 
 def decode(line):
     return expression.findall(line)
