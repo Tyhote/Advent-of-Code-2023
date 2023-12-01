2c3af9a5c50f0376614ca3c85d2d86f5074f2f96
Tyhote <hazelmadefresh@gmail.com>
2023-12-01 17:00:08 -0600

    Started work on the word to number conversion

--- Day-1/main.py
+++ Day-1/main.py
@@ -7,8 +7,17 @@ args = parser.parse_args()
 # This finds all numbers up to twenty.
 expression = re.compile(r'(?:(?:four|six|seven|nine)(?:teen)?|one|two|three|five|ten|eleven|twelve|thirteen|fifteen|eighteen|eight|twenty|\d)')
 
+def word_to_number(string):
+    
+
+
 def decode(line):
-    return expression.findall(line)
+    numbers = expression.findall(line)
+    if len(numbers) > 1:
+        numbers = [numbers[0], numbers[-1]]
+    else:
+        numbers = [numbers[0]]
+
 
 with open(args.input, "r") as inputfile:
     tot = 0
