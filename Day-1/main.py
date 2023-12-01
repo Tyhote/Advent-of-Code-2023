6040ec1850256783facb7d7e1d2191dd7b617aa9
Tyhote <hazelmadefresh@gmail.com>
2023-12-01 16:39:08 -0600

    Added regular expressions

--- Day-1/main.py
+++ Day-1/main.py
@@ -1,15 +1,17 @@
 import argparse
+import re
 parser = argparse.ArgumentParser("Day-1")
 parser.add_argument("input", help="The file to be processed", type=str)
-args = parser.parse_args()
 
+args = parser.parse_args()
+expression = re.compile(r'(?:(?:one|two|three|four|five|six|seven|eight)(?:teen)?|nine|ten|twenty|\d)')
 
 def decode(line):
-    numbers = [char for char in line if char.isdigit()]
-    return numbers[0] + numbers[-1]
+    return expression.findall(line)
 
 with open(args.input, "r") as inputfile:
     tot = 0
     for line in inputfile:
-        tot += int(decode(line))
+        numbers = decode(line)
+        tot += number for number in numbers
     print(tot)
